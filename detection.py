d7a711f4a4d68d7522a2c53cadf67880924c6b58
Leon Blumenthal <dev.blumenthal.leon@gmail.com>
2021-12-03 11:34:50 +0000

    split detection into model and loader

--- detection.py
+++ detection.py
@@ -1,10 +1,7 @@
-import os
 from dataclasses import dataclass
-from typing import Iterator, List, Tuple
+from typing import List, Tuple
 
 import numpy as np
-import torch
-from dotenv import load_dotenv
 
 
 @dataclass
@@ -16,42 +13,6 @@ class Detection:
     box: Tuple[float, float, float, float]
     label: int  # class
     score: float
-
-
-load_dotenv()
-
-
-_detections_path = os.getenv(
-    'DETECTIONS_PATH',
-    './data/detections',
-)
-if not os.path.exists(_detections_path):
-    raise Exception('Detections path is invalid')
-
-
-detection_list_paths = [
-    os.path.join(_detections_path, name)
-    for name in sorted(os.listdir(_detections_path))
-    if name.endswith('.detection')
-]
-
-
-def load_detection_list(index: int) -> List[Detection]:
-    """Load single detection list."""
-
-    detections = torch.load(detection_list_paths[index])
-
-    return detections
-
-
-def load_detection_lists(indices: List[int] = None) -> Iterator[List[Detection]]:
-    """Lazily load all detection lists."""
-
-    if indices is None:
-        indices = range(len(detection_list_paths))
-
-    for i in indices:
-        yield load_detection_list(i)
 
 
 def merge_masks(detections: List[Detection], dimensions: Tuple[int, int]) -> np.ndarray:
---
+++ detection_loader.py
@@ -0,0 +1,43 @@
+import os
+from typing import Iterator, List
+
+import torch
+from dotenv import load_dotenv
+
+from detection import Detection
+
+
+load_dotenv()
+
+
+_detections_path = os.getenv(
+    'DETECTIONS_PATH',
+    './data/detections',
+)
+if not os.path.exists(_detections_path):
+    raise Exception('Detections path is invalid')
+
+
+detection_list_paths = [
+    os.path.join(_detections_path, name)
+    for name in sorted(os.listdir(_detections_path))
+    if name.endswith('.detection')
+]
+
+
+def load_detection_list(index: int) -> List[Detection]:
+    """Load single detection list."""
+
+    detections = torch.load(detection_list_paths[index])
+
+    return detections
+
+
+def load_detection_lists(indices: List[int] = None) -> Iterator[List[Detection]]:
+    """Lazily load all detection lists."""
+
+    if indices is None:
+        indices = range(len(detection_list_paths))
+
+    for i in indices:
+        yield load_detection_list(i)
