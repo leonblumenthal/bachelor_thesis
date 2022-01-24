3ebb92ff1e5d0cd0216a3e9c2bdf5b1cff511d28
Leon Blumenthal <dev.blumenthal.leon@gmail.com>
2022-01-25 00:17:23 +0100

    updated detectron2 fps test with arguments and new loader

--- detectron2_fps_test.py
+++ detectron2_fps_test.py
@@ -1,17 +1,22 @@
+"""Load every image in a specified directory into memeory and measure inference fps."""
+
 import time
 import warnings
+from argparse import ArgumentParser
 
-import pcd_loader as pcd
 from detectron2_utils import load_default_predictor
-
+from loaders import FrameLoader
 
 # Ignore Detectron2 warning.
 warnings.simplefilter('ignore', UserWarning)
 
 
-def test_fps():
-    print('Loading images...')
-    images = list(pcd.load_images())
+def test_fps(src_dir: str):
+    loader = FrameLoader(src_dir)
+    print(f'Found {len(loader.paths)} frames')
+
+    print('Loading frames...')
+    images = list(loader.load_items())
 
     print('Loading model...')
     predictor = load_default_predictor()
@@ -23,12 +28,16 @@ def test_fps():
     elapsed_time = time.perf_counter() - start_time
 
     print(f'Elapsed time: {elapsed_time:.0f} s')
-    print(f'Number of images: {len(images)}')
-    print(f'Dimensions of images: {images[0].shape}')
+    print(f'Number of frames: {len(images)}')
+    print(f'Dimensions of frames: {images[0].shape}')
     print(
-        f'Performance: {len(images) / elapsed_time:.2f} fps, {elapsed_time / len(images):.2f} s/img'
+        f'Performance: {len(images) / elapsed_time:.2f} fps, {elapsed_time / len(images):.2f} seconds/frame'
     )
 
 
 if __name__ == '__main__':
-    test_fps()
+    parser = ArgumentParser()
+    parser.add_argument('src_dir', type=str)
+    args = parser.parse_args()
+
+    test_fps(args.src_dir)
