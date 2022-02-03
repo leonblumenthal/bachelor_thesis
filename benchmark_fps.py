58f2ed4c3e4cd1301db0edd1629aae0971336f9f
Leon Blumenthal <dev.blumenthal.leon@gmail.com>
2022-02-03 16:57:37 +0000

    improved fps test script

--- benchmark_fps.py
+++ benchmark_fps.py
@@ -1,11 +1,10 @@
-"""
-Load every frame in a specified directory into cpu memeory 
-and measure inference fps.
-"""
+"""Measure FPS using all frames in a directory."""
 
 import time
 import warnings
 from argparse import ArgumentParser
+
+import torch
 
 from src import detectron2_utils, yolact_utils
 from src.loaders import FrameLoader
@@ -14,20 +13,15 @@ from src.loaders import FrameLoader
 warnings.simplefilter('ignore', UserWarning)
 
 
-def test_fps(src_dir: str, model: str, config: str, weights_dir: str):
+def test_fps(
+    src_dir: str, model: str, config: str, weights_dir: str, num_warmup_frames: int = 10
+):
 
     frame_loader = FrameLoader(src_dir)
     num_frames = len(frame_loader.paths)
-
-    print(f'Loading {num_frames} frames...')
-
-    if num_frames == 0:
-        return
-
-    frames = list(frame_loader.load_items(rgb=False))
+    print(f'Found {num_frames} frames')
 
     print(f'Loading {model} model {config}...')
-
     if model == 'detectron2':
         if config == 'default':
             config = detectron2_utils.DEFAULT_CONFIG
@@ -35,19 +29,20 @@ def test_fps(src_dir: str, model: str, config: str, weights_dir: str):
     elif model == 'yolact':
         predictor = yolact_utils.load_predictor(config, weights_dir)
 
-    print('Runnning inference...')
-
-    start_time = time.perf_counter()
-    for frame in frames:
+    print('Running inference')
+    total_time = 0
+    for i, frame in enumerate(frame_loader.load_items(rgb=False)):
+        start_time = time.perf_counter()
         predictor(frame)
-    elapsed_time = time.perf_counter() - start_time
+        torch.cuda.synchronize()
+        end_time = time.perf_counter()
+        if i >= num_warmup_frames:
+            total_time += end_time - start_time
 
-    print(f'Elapsed time: {elapsed_time:.0f} s')
-    print(f'Number of frames: {num_frames}')
-    print(f'Dimensions of frames: {frames[0].shape}')
-    print(
-        f'Performance: {len(frames) / elapsed_time:.2f} fps, {elapsed_time / num_frames:.2f} seconds/frame'
-    )
+    fps = (num_frames - num_warmup_frames) / total_time
+    print('Finished')
+    print(f'Total seconds: {total_time:.02f}')
+    print(f'FPS: {fps:.02f}')
 
 
 if __name__ == '__main__':
